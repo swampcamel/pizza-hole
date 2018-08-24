259a47cfd9a8589095ba3f66f17bcd40dd3b67f5
Dan Mace <dmacebeta@gmail.com>
2018-08-24 11:35:25 -0700

    add sidebar for cart

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,15 @@ $(document).ready(function(){
 
   $('form#pizza-form').submit(function(event) {
     event.preventDefault();
-    console.log("button works");
-  })
+    var crust = $("input:radio[name=crust-group]:checked").val();
+    var size = $("input:radio[name=size-group]:checked").val();
+    var toppings = [];
+
+    $("input:checkbox[name=toppings-group]:checked").each(function() {
+     toppings.push($(this).val());
+   });
+
+   var newPizza = new Pizza(crust, size, toppings);
+   console.log(newPizza);
+  });
 });
