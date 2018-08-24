09f9b00ef4978ba4517bbbd5f5c3ba77e9c0b105
Dan Mace <dmacebeta@gmail.com>
2018-08-24 09:18:36 -0700

    add active form tab functionality to pizza nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+function Pizza(crustInput, sizeInput, toppingsInput) {
+  this.crust = crustInput;
+  this.size = sizeInput;
+  this.toppings = toppingsInput;
+}
+
   $(document).ready(function(){
     $('.sidenav').sidenav();
+
+    $('#pizza-nav').children().click(function() {
+      $(this).siblings().removeClass("active");
+      $(this).addClass("active");
+    });
   });
