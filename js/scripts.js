8e5baf87247027a5a2d7c151e63a7842fa33ecff
Dan Mace <dmacebeta@gmail.com>
2018-08-24 12:25:28 -0700

    add price calculation from form values that returns a total price for pizza

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,42 @@ function Pizza(crustInput, sizeInput, toppingsInput) {
   this.crust = crustInput;
   this.size = sizeInput;
   this.toppings = toppingsInput;
+  this.price = 0;
+}
+
+Pizza.prototype.pricePizza = function() {
+  console.log(this.toppings);
+  if (this.crust === "thin") {
+    this.price += 2;
+  } else if (this.crust === "stuffed") {
+    this.price += 4;
+  }
+  if (this.size === "small") {
+    this.price += 9;
+  } else if (this.size === "medium") {
+    this.price += 11;
+  } else if (this.size === "large") {
+    this.price += 13;
+  } else {
+    console.log("CATCH: scripts.js Line 21");
+  }
+
+  var toppingsArray = this.toppings;
+  var toppingsTotal = 0;
+  toppingsArray.forEach(function(topping) {
+    if(topping === "pepperoni") {
+      toppingsTotal++;
+    }
+    if (topping === "bacon") {
+      toppingsTotal += 2;
+    }
+    if (topping === "pineapple") {
+      toppingsTotal += 2;
+    }
+  });
+  this.price += toppingsTotal;
+
+  return this.price;
 }
 
 $(document).ready(function(){
@@ -24,5 +60,7 @@ $(document).ready(function(){
 
    var newPizza = new Pizza(crust, size, toppings);
    console.log(newPizza);
+   var calculateTotal = newPizza.pricePizza();
+   console.log(calculateTotal);
   });
 });
